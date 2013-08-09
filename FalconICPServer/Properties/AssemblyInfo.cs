7af42c5d4052841a2285cefd751ee7bef1ede644
Michał Janczykowski <mjanczykowski@gmail.com>
2013-08-09 04:14:32 +0200

    Set icon and app name

--- FalconICPServer/Properties/AssemblyInfo.cs
+++ FalconICPServer/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FalconICPServer")]
+[assembly: AssemblyTitle("Falcon ICP Server")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FalconICPServer")]
+[assembly: AssemblyProduct("Falcon ICP Server")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
